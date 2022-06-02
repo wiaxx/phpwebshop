5bb90fad01f5e1d3619b753c9069a433192c723c
Amanda <amanda.wikstrom@hotmail.com>
2022-06-02 16:18:17 +0200

    update path on requires in nested pages folder

--- pages/user/login.php
+++ pages/user/login.php
@@ -1,6 +1,5 @@
 <?php
-
-require_once __DIR__ . '/../classes/Template.php';
+require_once __DIR__ . '/../../classes/Template.php';
 
 Template::header('Login');
 
---
+++ pages/user/profile.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__ . '/../../classes/Template.php';
+
+Template::header('Profile page');
+
+Template::footer();
