8f3f48bd1806991d8672c3bed5157c1e2c84f147
Amanda <amanda.wikstrom@hotmail.com>
2022-06-02 14:07:46 +0200

    update css variables, add instructions, clean up Product.php

--- classes/Product.php
+++ classes/Product.php
@@ -4,11 +4,8 @@ class Product
 {
     public $id;
     public $name;
-    // public $color;
-    // public $size;
     public $description;
     public $price;
-    // public $article_nr;
 
     public function __construct($name, $description, $price, $id = 0)
     {
