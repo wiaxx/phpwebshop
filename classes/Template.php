cea9adb15e7324e77f5d9e5030b80b84a7da16a6
Amanda <amanda.wikstrom@hotmail.com>
2022-06-03 10:58:23 +0200

    small change in css and template nav

--- classes/Template.php
+++ classes/Template.php
@@ -23,7 +23,7 @@ class Template
                 <a href="/webshop">Home</a> |
                 <a href="/webshop/pages/products.php">Products</a> |
                 <a href="/webshop/pages/user/login.php">Login</a> |
-                <a href="/webshop/pages/user/register.php">Register</a>
+                <a href="/webshop/pages/user/register.php">Register</a> |
                 <a href="/webshop/pages/shopcart.php">Cart</a>
             </nav>
             <main>
