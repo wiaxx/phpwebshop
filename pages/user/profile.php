02e7365ae88d8a8fc266e071f7b720b93ed0b93a
Amanda <amanda.wikstrom@hotmail.com>
2022-06-06 20:37:40 +0200

    delete product as admin done

--- pages/user/profile.php
+++ pages/user/profile.php
@@ -21,28 +21,41 @@ if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
 
         <div class="admin-create-div">
 
+            <!-- form for create product and list all products below -->
             <div class="admin-div">
+
                 <h2>Create product</h2>
                 <form action="/webshop/scripts/product_create.php" method="post">
                     <input type="text" name="name" placeholder="Product name" required>
                     <input type="text" name="description" placeholder="Description">
-                    <input type="text" name="price" placeholder="Price" required>
+                    <input type="number" name="price" placeholder="Price" required>
                     <input type="file" name="picture">
                     <input type="submit" value="Create" class="btn btn-create">
                 </form>
 
                 <div>
                     <?php foreach ($products as $product) : ?>
-                        <p>
-                            <a href="/webshop/pages/product.php?id=<?= $product->id ?>">
+                        <div class="profile-show-all">
+                            <a href="/webshop/pages/product.php?id=<?= $product->id ?>" class="link">
                                 <?php echo $product ?>
                             </a>
-                        </p>
+
+                            <form action="/webshop/scripts/product_update.php" method="post">
+                                <input type="hidden" name="id" value="<?= $product->id ?>">
+                                <input type="submit" value="Edit" class="btn">
+                            </form>
+
+                            <form action="/webshop/scripts/product_delete.php" method="post">
+                                <input type="hidden" name="id" value="<?= $product->id ?>">
+                                <input type="submit" value="Delete" class="btn btn-delete">
+                            </form>
+                        </div>
                     <?php endforeach; ?>
                 </div>
             </div>
-
+            <!-- form for create user and list all users below -->
             <div class="admin-div">
+
                 <h2>Create user</h2>
                 <form action="/webshop/scripts/user_create.php" method="post">
                     <input type="text" name="username" placeholder="Username" required>
