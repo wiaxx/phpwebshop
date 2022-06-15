d9c4fe785b2c778c834dd8087768b5ab3d06483e
Amanda <amanda.wikstrom@hotmail.com>
2022-06-15 10:25:50 +0200

    show products in shop cart

--- pages/shopcart.php
+++ pages/shopcart.php
@@ -1,46 +1,52 @@
 <?php
-require_once __DIR__ . '/../classes/Template.php';
 require_once __DIR__ . '/../classes/Product.php';
-// require_once __DIR__ . '/../classes/Cart.php';
+require_once __DIR__ . '/../classes/Template.php';
 
 $is_logged_in = isset($_SESSION['logged_in']) && isset($_SESSION['user']);
 $logged_in_user = $is_logged_in ? $_SESSION['user'] : null;
 
+$products = isset($_SESSION['cart']) ? $_SESSION['cart'] : [];
+
 Template::header('Cart');
 
-// $shopcart = new Cart();
+if (empty($products)) { ?>
 
-// echo '<pre>';
-// print_r($shopcart->cart());
-// echo '</pre>';
+    <div class="cart">
+        <p>Your shopping cart is empty..</p>
+    </div>
 
-if (isset($_SESSION['cart']) && !empty($_SESSION['cart'])) {
+    <?php } else {
 
-    echo '<pre>';
-    print_r($_SESSION['cart']);
-    echo '</pre>';
+    foreach ($products as $product) : ?>
+
+        <div class="product-container">
+            <a href="/webshop/pages/product.php?id=<?= $product->id ?>">
+                <img src="<?= $product->img_url ?>" width="150" height="300" alt="Product image">
+            </a>
+            <b><?= $product->name ?></b>
+            <i><?= $product->price ?> kr</i> <br>
+        </div>
+
+    <?php
+    endforeach;
 
     if ($is_logged_in) { ?>
-        <div class="cart">
 
+        <div class="cart">
             <form action="/webshop/scripts/order_create.php" method="post">
                 <input type="hidden" name="customerID" value="<?= $logged_in_user->id ?>">
                 <input type="submit" value="Place order">
             </form>
         </div>
-    <?php    } else { ?>
+
+    <?php } else { ?>
+
         <div class="cart">
-            <p>
-                Please register or login to place order
-            </p>
+            <p>Please register or login to place order</p>
         </div>
-    <?php }
-} else { ?>
-    <div class="cart">
-        <p>
-            Your shopping cart is empty..
-        </p>
-    </div>
+
 <?php }
+}
+
 
 Template::footer();
